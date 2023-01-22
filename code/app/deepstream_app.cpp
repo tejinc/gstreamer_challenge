7177b23cd8a868d1e913f54c91b3ce95d4e45574
tejinc <tejinc.a1@gmail.com>
2023-01-22 14:20:55 -0500

    convert source code to c++, add callback function from deepstream_test1_app.c

--- code/app/deepstream_app.cpp
+++ code/app/deepstream_app.cpp
@@ -1,9 +1,11 @@
 #include <gst/gst.h>
 #include <glib.h>
-#include <stdio.h>
+#include <iostream>
 #include <cuda_runtime_api.h>
 #include "gstnvdsmeta.h"
 #include "nvds_yml_parser.h"
+
+
 
 #define MAX_DISPLAY_LEN 64
 
@@ -24,6 +26,80 @@ gint frame_number = 0;
 gchar pgie_classes_str[4][32] = { "Vehicle", "TwoWheeler", "Person",
   "Roadsign"
 };
+
+namespace callbacks {
+GstPadProbeReturn 
+  meta_parse(GstPad *pad, GstPadProbeInfo *info, gpointer u_data)
+  {
+      GstBuffer *buf = (GstBuffer *)info->data;
+      guint num_rects = 0;
+      NvDsObjectMeta *obj_meta = NULL;
+      guint vehicle_count = 0;
+      guint person_count = 0;
+      NvDsMetaList *l_frame = NULL;
+      NvDsMetaList *l_obj = NULL;
+      NvDsDisplayMeta *display_meta = NULL;
+
+      NvDsBatchMeta *batch_meta = gst_buffer_get_nvds_batch_meta(buf);
+
+      /* ============================ 
+       * parser from deepstream_test1_app.c */
+      for (l_frame = batch_meta->frame_meta_list; l_frame != NULL;
+        l_frame = l_frame->next) {
+          NvDsFrameMeta *frame_meta = (NvDsFrameMeta *) (l_frame->data);
+          int offset = 0;
+          for (l_obj = frame_meta->obj_meta_list; l_obj != NULL;
+                  l_obj = l_obj->next) {
+              obj_meta = (NvDsObjectMeta *) (l_obj->data);
+              if (obj_meta->class_id == PGIE_CLASS_ID_VEHICLE) {
+                  vehicle_count++;
+                  num_rects++;
+              }
+              if (obj_meta->class_id == PGIE_CLASS_ID_PERSON) {
+                  person_count++;
+                  num_rects++;
+              }
+          }
+          display_meta = nvds_acquire_display_meta_from_pool(batch_meta);
+          NvOSD_TextParams *txt_params  = &display_meta->text_params[0];
+          display_meta->num_labels = 1;
+          txt_params->display_text = static_cast<char*>(g_malloc0 (MAX_DISPLAY_LEN));
+          offset = snprintf(txt_params->display_text, MAX_DISPLAY_LEN, "Person = %d ", person_count);
+          offset = snprintf(txt_params->display_text + offset , MAX_DISPLAY_LEN, "Vehicle = %d ", vehicle_count);
+
+          /* Now set the offsets where the string should appear */
+          txt_params->x_offset = 10;
+          txt_params->y_offset = 12;
+
+          /* Font , font-color and font-size */
+          txt_params->font_params.font_name = const_cast<char*>(std::string("Serif").c_str());
+          txt_params->font_params.font_size = 10;
+          txt_params->font_params.font_color.red = 1.0;
+          txt_params->font_params.font_color.green = 1.0;
+          txt_params->font_params.font_color.blue = 1.0;
+          txt_params->font_params.font_color.alpha = 1.0;
+
+          /* Text background color */
+          txt_params->set_bg_clr = 1;
+          txt_params->text_bg_clr.red = 0.0;
+          txt_params->text_bg_clr.green = 0.0;
+          txt_params->text_bg_clr.blue = 0.0;
+          txt_params->text_bg_clr.alpha = 1.0;
+
+          nvds_add_display_meta_to_frame(frame_meta, display_meta);
+      }
+
+      g_print ("Frame Number = %d Number of objects = %d "
+              "Vehicle Count = %d Person Count = %d\n",
+              frame_number, num_rects, vehicle_count, person_count);
+      frame_number++;
+
+
+
+      return GST_PAD_PROBE_OK;
+  }
+}
+
 
 
 
@@ -117,6 +193,7 @@ main (int   argc, char *argv[])
     g_printerr ("One element could not be created. Exiting.\n");
     return -1;
   }
+
 
 
 
@@ -216,6 +293,10 @@ main (int   argc, char *argv[])
   }
 
   /* Add meta data here TODO */
+  GstPad *probe_pad = NULL;
+  std::string pad_name = "sink";
+  probe_pad = gst_element_get_static_pad( nvosd, pad_name.c_str());
+  gst_pad_add_probe(probe_pad, GST_PAD_PROBE_TYPE_BUFFER, &callbacks::meta_parse, NULL, NULL);
 
   /* Set the pipeline to "playing" state */
   g_print ("Using file: %s\n", argv[1]);
@@ -234,3 +315,4 @@ main (int   argc, char *argv[])
   g_main_loop_unref (loop);
   return 0;
 }
+
--- code/app/run_test.sh
+++ code/app/run_test.sh
@@ -1,5 +1,5 @@
 export SAMPLE_VIDEO=/opt/nvidia/deepstream/deepstream/samples/streams/sample_720p.h264
-export OUT_VIDEO=test.avi
+export OUT_VIDEO=test.h264
 export NVINFER_YML=/code/config_infer_primary.yml
 export TRACKER_YML=/opt/nvidia/deepstream/deepstream-6.1/samples/configs/deepstream-app/config_tracker_NvDCF_perf.yml
 export TRACKER_LIB=/opt/nvidia/deepstream/deepstream/lib/libnvds_nvmultiobjecttracker.so
